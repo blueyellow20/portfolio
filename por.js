ced6710b11ad48c47ce7e0a3d6560b974fb8e9db
blueyellow20 <72426699+blueyellow20@users.noreply.github.com>
2020-11-10 09:24:27 +0800

    popup
    popup

--- por.js
+++ por.js
@@ -5,3 +5,7 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
+}
